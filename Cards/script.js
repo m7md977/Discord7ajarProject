c97e5a1a461b09a4427e2e6d42b6abd87da24f28
Mohamad Alayoub <39346485+m7md977@users.noreply.github.com>
2023-09-22 13:04:43 +0200

    cards2

--- Cards/script.js
+++ Cards/script.js
@@ -1,3 +1,34 @@
+// Function to fetch and display user data
+function fetchUserData() {
+  fetch("random_users.json") // Replace 'random_users.json' with your JSON file's path
+    .then((response) => response.json())
+    .then((data) => {
+      const userDataDiv = document.getElementById("user-data");
+      userDataDiv.innerHTML = ""; // Clear existing content
+
+      data.forEach((user) => {
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+                          <div class="card-content">
+                            <img src="${user.img}" alt="">
+                            <h2>${user.name}</h2>
+                            <p><strong>Email:</strong> ${user.email}
+                            <br><strong>Username:</strong> ${user.username}
+                            <br><strong>Age:</strong> ${user.age}
+                            <br><strong>Address:</strong> <br> ${user.address.street}, <br>${user.address.city}, <br>${user.address.state} ${user.address.zipcode}</p>
+                          </div>
+                        `;
+        userDataDiv.appendChild(userDiv);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching user data:", error);
+    });
+}
+
+// Call the fetchUserData function when the page loads
+window.onload = fetchUserData;
+
 function fetchRandomUser() {
   fetch("https://randomuser.me/api/")
     .then((response) => response.json())
